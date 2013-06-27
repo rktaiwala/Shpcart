0b78c33389caf3d9fd59ae32a2d782f64aee00b7
Rahul Taiwala <rktaiwala@gmail.com>
2013-06-27 12:29:51 +0530

    Update ShpcartViewController.php

--- src/controllers/ShpcartViewController.php
+++ src/controllers/ShpcartViewController.php
@@ -6,12 +6,12 @@
 
 
 /**
- * Libraries we can use.
+ * Libraries we use.
  */
 use Shpcart\Model\Products;
-use Illuminate\Support\Facades\View;
-use Illuminate\Support\Facades\Input;
-use Illuminate\Support\Facades\Redirect;
+use \View;
+use \Input;
+use \Redirect;
 /**
  * The product information page.
  */
